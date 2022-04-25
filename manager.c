9c5a6609d7a7b51a6d7860ec3da17a2ee94651ef
Kyutae02 <boxo8707@gamil.com>
2022-04-25 21:45:18 +0900

    loadData

--- manager.c
+++ manager.c
@@ -51,14 +51,22 @@ FILE *fp;
 
 //파일에서 데이터 불러오는 함수
 int loadData(Product *p){
-	int count=0;
-	FILE*fp;
+int count = 0;
+ FILE *fp;
 
-	//파일 내용을 읽어와서 배열에 값 추가하기
-
-
-
-
-	printf("=> 로딩 성공!\n");
-	return count;
+ fp = fopen("Product.txt","rt");
+ if(fp==NULL) printf("=>파일 없음\n");
+ else{
+ for(;count<100;count++)
+ {
+ fscanf(fp,"%d",&p[count].weight);
+ if(feof(fp)) break;
+ fscanf(fp,"%d",&p[count].price);
+ fscanf(fp,"%[^\n]s",p[count].name);
+ }
+ fclose(fp);
+ printf("=>로딩 성공!");
+printf("%d",count);
+}
+ return count;
 }
